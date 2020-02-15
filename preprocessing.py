c70ba081feafc47d9cecf609c8c9a83cf0dfabbe
ZaeemQ <zaeem107@gmail.com>
2020-02-15 00:29:46 -0500

    finished module 1

--- preprocessing.py
+++ preprocessing.py
@@ -2,11 +2,30 @@
 from bs4 import BeautifulSoup
 import urllib
 import codecs
+import json
 
-
-f = open("corpus.html")
+f = open("corpustest.html")
 content = f.read()
 soup = BeautifulSoup(content, 'html.parser')
 
-print(soup.prettify())
+# print(soup.prettify())
 
+docid = 0
+data = {}
+
+
+main_table = soup.find_all("div", attrs={'class': 'courseblock'})
+for course in main_table:
+    docid += 1
+    test = course.text
+    data[str(docid)] = []
+    data[str(docid)].append(course.text)
+    print("------")
+    print(course.text)
+
+docnum = docid
+
+print(docnum)
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
